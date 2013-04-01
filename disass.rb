8f582e09ccc9c19e69bf4f9f3267559c13c49659
Dean Ancajas <dbancajas@aggiemail.usu.edu>
2013-03-31 23:03:48 -0600

    operand 2, data processing instructions complete

--- disass.rb
+++ disass.rb
@@ -25,7 +25,7 @@ $MUL_mask = sum
 
 #mask for Data Processing Instructions
 DP_mask_list = (26..27).to_a
-DP_pattern = 0b00<<26 #is just = zero
+DP_pattern = (0b00)<<26 #is just = zero
 
 sum = 0
 DP_mask_list.each do | n | 
@@ -70,6 +70,7 @@ end
 def getInstruction(io)
 	y = InstBits.read(io).ins.to_i
 	x = InstBits.read(io).ins.to_i
+
 	(x<<16) + y
 end
 
@@ -90,16 +91,142 @@ end
 def is_a_SWP? (ins)
 	abort unless ins.is_a? Integer
 	tmp = (ins & $SWP_mask)
-	puts "#{tmp} and #{SWP_pattern} and #{ins.to_s(16)}"
+	#puts "#{tmp} and #{SWP_pattern} and #{ins.to_s(16)}"
 	SWP_pattern == tmp	
 end
 
+def condition(ins)
+	tmp = (ins>>28) & (0xF)
+	
+	if (tmp == 0)
+		"EQ"
+	elsif (tmp == 1)
+		"NE"
+	elsif (tmp == 2)
+		"CS"
+	elsif (tmp == 3)
+		"CC"
+	elsif (tmp == 4)
+		"MI"
+	elsif (tmp == 5)
+		"PL"
+	elsif (tmp == 6)
+		"VS"
+	elsif (tmp == 7)
+		"VC"
+	elsif (tmp == 8)
+		"HI"
+	elsif (tmp == 9)
+		"LS"
+	elsif (tmp == 10)
+		"GE"
+	elsif (tmp == 11)
+		"LT"
+	elsif (tmp == 12)
+		"GT"
+	elsif (tmp == 13)
+		"LE"
+	elsif (tmp == 14)#always, but we don't have to put it
+		""
+	elsif (tmp == 15)
+		"NV"
+	end
+end
+
+def S_update? (ins)
+	abort unless ins.is_a? Integer
+	((ins >> 20) & 0b1) == 1
+end
+
+def getRd(ins) #destination register
+	abort unless ins.is_a? Integer
+	rd_position = 12
+	("r" + ((ins >> rd_position) & 0xF).to_i.to_s)
+end
+
+def getRn(ins) #first operand
+	abort unless ins.is_a? Integer
+	rn_position = 16
+	("r" + ((ins >> rn_position) & 0xF).to_i.to_s)
+end
+
+def getR2(ins) #second operand register only
+	true
+end
+
+def opcode_DP(ins)
+	tmp =( (ins>>21) & (0xF) )
+
+	if (tmp == 0x0)
+		"AND"
+	elsif (tmp == 0b0001)
+		"EOR"
+	elsif (tmp == 0b0010)
+		"SUB"
+	elsif (tmp == 0b0011)
+		"RSB"
+	elsif (tmp == 0b0100)
+		"ADD"
+	elsif (tmp == 0b0101)
+		"ADC"
+	elsif (tmp == 0b0110)
+		"SBC"
+	elsif (tmp == 0b0111)
+		"RSC"
+	elsif (tmp == 0b1000)
+		"TST"
+	elsif (tmp == 0b1001)
+		"TEQ"
+	elsif (tmp == 0b1010)
+		"CMP"
+	elsif (tmp == 0b1011)
+		"CMN"
+	elsif (tmp == 0b1100)
+		"ORR"
+	elsif (tmp == 0b1101)
+		"MOV"
+	elsif (tmp == 0b1110)
+		"BIC"
+	elsif (tmp == 0b1111)
+		"MVN"
+	else 
+		puts "unknown Data Processing instruction"
+		abort
+	end
+end
+
+def matchp (st,en,ins,pattern) # matches with start,end bits, with the pattern
+	abort unless pattern.is_a? Integer
+	abort unless ins.is_a? Integer
+	abort unless st.is_a? Integer
+	abort unless en.is_a? Integer
+
+	mask = 1
+	(1...(en-st+1)).each do | x |
+		mask = (mask<<1) + 1		
+	end
+
+	tmp = (ins>>st) & mask
+	tmp == pattern
+	
+end
+
+def extract (st,en,ins)
+	mask = 1
+	(1...(en-st+1)).each do | x |
+		mask = (mask<<1) + 1		
+	end
+	((ins>>st) & mask)
+end
+
 # open file
-io = File.open("bin/swp.bin","rb")
+io = File.open("bin/and.bin","rb")
 
 
-(1..6).each do | x |
+while !io.eof? # read until there are no more instructions
+
 	inst = getInstruction(io)
+	
 	inst_str = inst.to_s(16)
 
 	if is_a_MUL? (inst)
@@ -107,8 +234,67 @@ io = File.open("bin/swp.bin","rb")
 	elsif is_a_SWP?(inst)
 		puts "#{inst_str} is a Swap Instruction"
 	elsif is_a_DP?(inst)
-		puts "#{inst_str} is a Data Processing"	
+		#puts "#{inst_str} is a Data Processing"	
+		opc = opcode_DP(inst)
+
+
+
+		oprd = "r" + extract(12,15,inst).to_s
+		oprn = "r" + extract(16,19,inst).to_s
+		oprm = "r" + extract(0,3,inst).to_s
+		opr2=""
+		#check type of Operand2 when not immediate
+		if (matchp(25,25,inst,1))# this is an immediate value
+			rotateval = extract(8,11,inst) * 2 #must be x2 due to even number shifting
+			baseval = extract(0,7,inst)
+
+			unless rotateval == 0
+				val = (baseval<<32) >> rotateval
+			else
+				val = baseval
+			end
+			
+
+			oprm = "#" + val.to_s #this is an imm value
+		elsif matchp(4,4,inst,0)
+			#puts "immediate shift"
+			immval = extract(7,11,inst)
+			if (immval > 0)
+				if matchp(5,6,inst,0b00)
+					opr2 += ", LSL "
+				elsif matchp(5,6,inst,0b01)
+					opr2 += ", LSR "
+				elsif matchp(5,6,inst,0b10)
+					opr2 += ", ASR "
+				elsif matchp(5,6,inst,0b11)
+					opr2 += ", ROR "
+				end 
+				opr2 += "#" + extract(7,11,inst).to_s
+			end
+		else
+			if matchp(5,6,inst,0b00)
+				opr2 += ", LSL "
+			elsif matchp(5,6,inst,0b01)
+				opr2 += ", LSR "
+			elsif matchp(5,6,inst,0b10)
+				opr2 += ", ASR "
+			elsif matchp(5,6,inst,0b11)
+				opr2 += ", ROR "
+			end
+			opr2 += "r" + extract(8,11,inst).to_s
+		end
+		cond = condition(inst)
+
+		unless matchp(20,20,inst,1)
+			status=""
+		else
+			status="s"
+		end	
+		
+		print "#{opc}#{status}#{cond} #{oprd}, #{oprn}, #{oprm}#{opr2}\n"
 	end
+	
+	
 end
 
 #get 1 instruction and analyze it
--- src/and.s
+++ src/and.s
@@ -1,8 +1,26 @@
 .global _start
 
 _start:
-	AND r1,r0,#2
-	AND r1,r0,r3
-	MOV r5,#9
-	AND r1,r2,ROR r5
-	MUL r1,r2,r0
+	AND r10,r7,#256
+	AND r10,r7,#16
+	AND r10,r7,#32
+	AND r10,r7,#64
+	AND r10,r7,#128
+	AND r10,r7,#1024
+	AND r10,r7,#51200
+	AND r15,r6,r8
+	ANDEQS r1,r2,ROR r5
+	AND r9,r6,r8
+	AND r11,r6,r7
+	AND r12,r6,r6
+	AND r13,r6,r5
+	AND r14,r6,r4
+	AND r9,r6,r8, LSL #16
+	AND r9,r6,r8, LSR #12
+	AND r9,r6,r8, ROR #5
+	AND r9,r6,r8, ASR #2
+	AND r9,r6,r8, ROR #0
+	AND r9,r6,r8, ASR R10
+	ADD r9,r6,r8, ASR R10
+	ADD r10,r7,#51200
+	ADD r10,r7,#16
